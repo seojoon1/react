0ceb478d65b3eb1b466f1d128cfc2dc4bd76e8ff
comppy <kim1122aass@gmail.com>
2025-06-22 00:50:10 +0900

    zehahahaha

--- NewNeoCNJ/src/data.jsx
+++ NewNeoCNJ/src/data.jsx
@@ -4,7 +4,8 @@ id : 0,
 img : "/test1.png",
 title : "김송글",
 content : "Born in France",
-price : 120000
+price : 120000,
+volume : 5
 },
 
 {
@@ -12,7 +13,8 @@ img : "/test2.png",
 id : 1,
 title : "김똥글",
 content : "Born in Seoul",
-price : 110000
+price : 110000,
+volume : 5
 },
 
 {
@@ -20,7 +22,24 @@ price : 110000
 id : 2,
 title : "이송글",
 content : "Born in the States",
-price : 130000
+price : 130000,
+volume : 5
+},
+{
+img : "/test4.png",
+id : 3,
+title : "고차원",
+content : "높은 차원이다",
+price : 1300000000,
+volume : 0
+},
+{
+img : "/test5.png",
+id : 4,
+title : "유태정",
+content : "유탱정이다다",
+price : 155788848,
+volume : 0
 }
 ] 
 export default data;
--- NewNeoCNJ/src/detail1.jsx
+++ NewNeoCNJ/src/detail1.jsx
@@ -8,13 +8,15 @@ function detail(props){
     <div className="detail">
         <div className="row">
             <div className="col-md-6">
-                <img src={props.goods[id].img}/>
-        </div>
+                <img src={props.goods[id].volume == 0 ? '/soldout.jpg' : props.goods[id].img}/>
+            </div>
             <div className="col-md-6">
                 <h4 className="pt-5">{props.goods[id].title}</h4>
                 <p>{props.goods[id].content}</p>
                 <p>{props.goods[id].price}</p>
-                <button className="btn btn-danger">주문하기</button> 
+                <button className="btn btn-danger" onClick={()=>{
+                    alert("기다려주세요")
+                }}>주문하기</button> 
             </div>
         </div>
     </div> 
